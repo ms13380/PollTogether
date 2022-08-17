b9310446e089996dd88f8d2b88f5ae12d259f9bd
Jared Daniel <60400956+jareddaniel95@users.noreply.github.com>
2022-08-17 19:55:12 -0400

    Fix broken connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,17 @@
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(process.env.JAWSDB_URL || process.env.MYSQL_URI)
-
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL)
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: '127.0.0.1',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306,
     }
