fb0fdd5705db65a9698a5facd05c5dd7caaea4aa
Bocar Ly <bl-engineer@Bocars-iMac.attlocal.net>
2022-08-17 15:31:27 -0400

    User linked to Answer by adding references

--- models/Answer.js
+++ models/Answer.js
@@ -17,7 +17,11 @@ Answer.init(
         user_id: {
             type: DataTypes.INTEGER,
             allowNull: false,
-            primaryKey: true
+            // primaryKey: true
+            references: {
+                model: 'user',
+                key: 'id'
+            }
         },
         option: {
             type: DataTypes.STRING,
