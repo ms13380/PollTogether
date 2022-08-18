4421ede3ef849ef32c2fe8aa811e30e9d231aec3
VT_AOE_91 <amartin80108@gmail.com>
2022-08-17 21:14:56 -0400

    Added functionality for dashboard

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const dashRoutes = require('./dashboard-routes.js')
 // const userPageRoutes = require('./user-page-routes.js');
 
 // router.use('/user-page', userPageRoutes);
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashRoutes);
 
 
 module.exports = router;
