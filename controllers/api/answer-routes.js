a90dbcf9a4ee565dada3ba3a27276c15b9d5d8e6
Jared Daniel <60400956+jareddaniel95@users.noreply.github.com>
2022-08-17 23:30:02 -0400

    Answer post route now validates expiration date, hide expiration date on poll page if null

--- controllers/api/answer-routes.js
+++ controllers/api/answer-routes.js
@@ -4,6 +4,13 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
     try {
+        const poll = await Poll.findByPk(req.body.poll_id, {
+        });
+        const expiration = poll.poll_expire ? new Date(new Date(poll.poll_expire).toUTCString()) : null;
+        const today = new Date(new Date().toUTCString());
+        if (expiration && expiration > today) {
+            throw 'Poll expired.';
+        }
         const data = await Answer.create({
             poll_id: req.body.poll_id,
             user_id: req.session.user_id,
