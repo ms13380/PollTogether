de6740227f68b6a0ab6da4c45126ee473a2777f7
Jared Daniel <60400956+jareddaniel95@users.noreply.github.com>
2022-08-18 21:01:20 -0400

    Add styling

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
 
 app.use(routes);
 
